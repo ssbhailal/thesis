b6f5d2e03e67de61f11971d6739264f38db3597e
ssbhailal <shaya@Laptop-van-Shaya.localdomain>
2025-06-06 23:31:25 +0200

    Fixed ratings, now numeric

--- final_script.py
+++ final_script.py
@@ -1,4 +1,4 @@
-# Filename: script.py
+# Filename: final_script.py
 # Description: A script to train the model and clasify 1000 discussions
 # Date: 
 # Authors: Shaya Bhailal
@@ -7,7 +7,7 @@ import json
 import pandas as pd
 import numpy as np
 import spacy
-import re
+import sys
 from scipy.sparse import csr_matrix
 from empath import Empath
 from sklearn.dummy import DummyClassifier
@@ -26,7 +26,7 @@ from sklearn.metrics import (
 
 
 def load_data(path):
-    """Load json file and return in pandas dataframe."""
+    """Load json file and return pandas dataframe."""
     print('Loading ' + path + '...')
     with open(path, 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -37,13 +37,13 @@ def load_data(path):
 def convert_to_categories_series(series, num):
     """Convert Likert scale to ether 3 or 2 categroies, depending on num."""
     if num == 3:
-        return series.map(lambda x: 'low' if x in [1, 2] else 'medium' if x == 3 else 'high' if x in [4, 5] else 'unk')
+        return series.map(lambda x: 1 if x in [1, 2] else 2 if x == 3 else 3 if x in [4, 5] else 'unk')
     elif num == 2:
-        return series.map(lambda x: 'low' if x in [1, 2]  else 'high' if x in [3, 4, 5] else 'unk')
+        return series.map(lambda x: 1 if x in [1, 2]  else 3 if x in [3, 4, 5] else 'unk')
 
 
-def convertion(data, num):
-    """Puts convertion all together by going through the labels."""
+def conversion(data, num):
+    """Puts conversion all together by going through the labels."""
     data['suspense'] = convert_to_categories_series(data['suspense'], num)
     data['curiosity'] = convert_to_categories_series(data['curiosity'], num)
     data['surprise'] = convert_to_categories_series(data['surprise'], num)
@@ -51,25 +51,27 @@ def convertion(data, num):
 
 
 def create_model():
-    """Creates the ensemble model."""
+    """Creates the ensemble model, choice from SVM, LR, NB and RF."""
     svm_cls = SVC(kernel='linear', random_state=42, C=10, probability=True)
     lr_cls = LogisticRegression(max_iter=1000, random_state=42)
     nb_cls = MultinomialNB()
     rf_cls = RandomForestClassifier(n_estimators=100, random_state=42)
 
-    voting_cls = VotingClassifier(estimators=[
+    voting_cls = VotingClassifier(estimators=[ # Adjust to include other models
         # ('nb_cls', nb_cls),
         ('svm_cls', svm_cls),
         # ('lr_cls', lr_cls),
         ('rf_cls', rf_cls)
     ], voting='soft')
+    
+    # Handling multi ouputs
     multi_output_model = MultiOutputClassifier(voting_cls)
     return multi_output_model
 
 
 
 def extract_spacy_features(texts):
-    """Ëxtracts n_sentences, n_entities, num_questions, lexical_diversity, avg_token_len."""
+    """Extracts n_sentences, n_entities, num_questions, lexical_diversity, avg_token_len."""
     nlp = spacy.load('en_core_web_lg')
     nlp.add_pipe("sentencizer")
     n_entities = []
@@ -92,7 +94,7 @@ def extract_spacy_features(texts):
 
 
 def extract_empath_features(texts, categories=None):
-    """Extracts emotion per text."""
+    """Extracts emotion per text using Empath libary."""
     lexicon = Empath()
     if categories is None:
         categories = list(lexicon.cats)
@@ -126,24 +128,37 @@ def baseline(test):
         print('F1 weighted:', f1_score(y_test, pred, average='weighted'))
     
 
+def show_misclassified(y_test, y_pred, X_test):
+    for idx, (true, pred) in enumerate(zip(y_test, y_pred)):
+        if (true != pred).any():
+            print(f'Index: {idx}')
+            print(f'Text: {X_test[idx]}')
+            print(f'True label: {true}')
+            print(f'Predicted label: {pred}')
+            print('-'*80)
+
 
 def train_test(model, train, test):
     """"Train and test on annotated data."""
     print('Prepping data...')
+    # Convert story to numeric
     train['story_class'] = train['story_class'].map({'Story': 1, 'Not Story': 0})
     test['story_class'] = test['story_class'].map({'Story': 1, 'Not Story': 0})
     X_train = train['body'].tolist()
     y_train = train[['suspense', 'curiosity', 'surprise', 'story_class']].values
     X_test = test['body'].tolist()
     y_test = test[['suspense', 'curiosity', 'surprise', 'story_class']].values
+    # TF-IDF
     tfidf = TfidfVectorizer()
     X_train_tfidf = tfidf.fit_transform(X_train)
     X_test_tfidf = tfidf.transform(X_test)
- 
+    # Empath
     train_empath_features = extract_empath_features(X_train)
     test_empath_features = extract_empath_features(X_test)
+    # Lexical features
     train_spacy_features = extract_spacy_features(X_train)
     test_spacy_features = extract_spacy_features(X_test)
+    # Combine with hstack
     X_train_combined = hstack([X_train_tfidf, train_spacy_features, train_empath_features])
     X_test_combined = hstack([X_test_tfidf, test_spacy_features, test_empath_features])
     print('Now training...')
@@ -151,10 +166,8 @@ def train_test(model, train, test):
     print('Predicting...')
     y_pred = model.predict(X_test_combined)
     evaluate(y_test, y_pred)
-    label_names = ['suspense', 'curiosity', 'surprise', 'story_class']
-    for i, label in enumerate(label_names):
-        print(f"\nClassification report for {label}:")
-        print(classification_report(y_test[:, i], y_pred[:, i], zero_division=0))
+    # Uncomment to see misclassifications by model v v v
+    # show_misclassified(y_test, y_pred, X_test)
     return model, tfidf
 
   
@@ -167,7 +180,7 @@ def evaluate(labels, pred):
         accuracy = accuracy_score(labels[:, i], pred[:, i])
         print(f"Accuracy: {accuracy:.3f}")
 
-        precision = precision_score(labels[:, i], pred[:, i], average="weighted")
+        precision = precision_score(labels[:, i], pred[:, i], average="weighted", zero_division=0)
         print(f"Precision: {precision:.3f}")
 
         recall = recall_score(labels[:, i], pred[:, i], average="weighted")
@@ -175,30 +188,31 @@ def evaluate(labels, pred):
 
         f_score = f1_score(labels[:, i], pred[:, i], average="weighted")
         list.append(f_score)
-        print(f"F1-score: {f_score:.3f}")
+        print(f"F1-score(weighted): {f_score:.3f}")
+        
+        f_score2 = f1_score(labels[:, i], pred[:, i], average="macro")
+        list.append(f_score2)
+        print(f"F1-score(macro): {f_score2:.3f}")
+        
     print('Average Fscore:', sum(list) / len(list))
 
 
 def process(data):
     """Prepare sample for classifying."""
+    # Extracting relevant columns
     df = data[['name', 'author', 'body', 'num_comments', 'delta', 'selftext']].copy()
     df.loc[df['body'] == '', 'body'] = data.loc[df['body'] == '', 'selftext']
-    df['body'] = df['body'].str.replace(
-    '_____  &gt; \*Hello, users of CMV!.*\*Happy CMVing!\*',
-    '', regex=True)
+    # Filter comments
     df = df[df['author'] != 'DeltaBot']
     df = df[~df['body'].isin(['[deleted]', '[redacted]', '[removed]'])]
+    # Delete selftext, everyting needed is now in 'body'
     df.drop(columns=['selftext'], inplace=True)
-    df.to_json('sample_cls.json', orient='records', lines=True)
+    df.to_json('sample_cls1.json', orient='records', lines=True)
     return df
 
 
 def classify(model, data, tfidf):
     """Classifies sample."""
-    delta, no_delta = persuasion(data)
-    mean_nonzero = data[data['num_comments'] != 0]['num_comments'].mean()
-    high_int = data[data['num_comments'] > mean_nonzero]
-    low_int = data[data['num_comments'] < mean_nonzero]
     texts = data['body'].tolist()
     print('Extracting features...')
     texts_tfidf = tfidf.transform(texts)
@@ -208,24 +222,31 @@ def classify(model, data, tfidf):
     print('Classifying sample...')
     predictions = model.predict(texts_combined)
     label_names = ['suspense', 'curiosity', 'surprise', 'story']
-
+    print('Labelling..')
     for i, label in enumerate(label_names):
         data[f'predicted_{label}'] = predictions[:, i]
-    data.to_json('sample_cls.json', orient='records', lines=True)
+    data.to_json('sample_cls1.json', orient='records', lines=True)
     return
 
 
-def main():
+def main(argv):
     train_file = 'golden-standard-train.json'
     test_file = 'golden-standard-test.json'
     data_file = 'threads1000_format.json'
+    if len(sys.argv) < 2:
+        print('Please specify how many labels you want the model to train on. Options are 2, 3 and 5')
+        exit(-1)
+    mapping = int(argv[1])
     train_data = load_data(train_file)
     test_data = load_data(test_file)
-    # train_data = convertion(train_data, 3)
-    # test_data = convertion(test_data, 3)
-    # model = create_model()
-    # trained_model, tfidf = train_test(model, train_data, test_data)
-    baseline(test_data)  
+    # Model training and evaluation
+    if mapping != 5:
+        train_data = conversion(train_data, mapping)
+        test_data = conversion(test_data, mapping)
+    model = create_model()
+    trained_model, tfidf = train_test(model, train_data, test_data)
+    baseline(test_data)
+    # Automatic classification
     discussions1k = load_data(data_file)
     data = process(discussions1k)
     classify(model, data, tfidf)
@@ -233,4 +254,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
